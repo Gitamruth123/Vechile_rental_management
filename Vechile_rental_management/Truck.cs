7d1433f912f1969107797a27b7a18cd4b1320e7e
Gitamruth123 <amruthav1991@gmail.com>
2024-07-17 15:54:35 -0400

    Truck class

--- Vechile_rental_management/Truck.cs
+++ Vechile_rental_management/Truck.cs
@@ -1,12 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Vechile_rental_management
+public class Truck : Vehicle
 {
-    internal class Truck
-    {
-    }
+  public double Capacity{ get; set; }
+  public string TruckType{ get; set; }
+  public bool FourWheelDrive{ get; set; }
+
+public Truck(string model, string manufacturer, int year, decimal rentalPrice, double capacity, string truckType, bool fourWheelDrive)
+        : base(model, manufacturer, year, rentalPrice)
+  {
+    Capacity = capacity;
+    TruckType = truckType;
+    FourWheelDrive = fourWheelDrive;
+  }
+ public override void DisplayDetails()
+   {
+     base.DisplayDetails();
+     Console.WriteLine("Capacity: " + Capacity + " tons, Truck Type: " + TruckType + ", Four Wheel Drive: " + FourWheelDrive);
+   }
 }
+
