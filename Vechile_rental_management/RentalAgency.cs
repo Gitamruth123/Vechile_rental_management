a4877d12f07ff713f92b4c1e787bd4b2de394c9b
Gitamruth123 <amruthav1991@gmail.com>
2024-07-17 19:07:55 -0400

    RentalAgency Class

--- Vechile_rental_management/RentalAgency.cs
+++ Vechile_rental_management/RentalAgency.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Vechile_rental_management
+public class RentalAgency
 {
-    internal class RentalAgency
-    {
-    }
+  private List<Vehicle> Fleet{ get; set; }
+  public decimal TotalRevenue{ get; private set; }
+
+  public RentalAgency()
+  {
+     Fleet = new List<Vehicle>();
+     TotalRevenue = 0;
+  }
+
+  public void AddVehicle(Vehicle vehicle)
+  {
+     Fleet.Add(vehicle);
+     Console.WriteLine(vehicle.Model + " added to fleet.");
+  }
+
+  public void RemoveVehicle(Vehicle vehicle)
+  {
+     Fleet.Remove(vehicle);
+     Console.WriteLine(vehicle.Model + " removed from fleet.");
+  }
+  public void RentVehicle(Vehicle vehicle, int days)
+  {
+     if (Fleet.Contains(vehicle))
+     {
+         decimal rentalCost = vehicle.RentalPrice * days;
+         TotalRevenue += rentalCost;
+         Console.WriteLine(vehicle.Model + " rented for " + days + " days at a cost of " + rentalCost.ToString("C") + ". Total revenue: " + TotalRevenue.ToString("C"));
+     }
+     else
+     {
+        Console.WriteLine(vehicle.Model + " is not available in the fleet.");
+     }
+  }
 }
