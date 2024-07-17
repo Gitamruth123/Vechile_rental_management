f9a28f551e6b3f3fed2db46dec0704bf53b135ad
Gitamruth123 <amruthav1991@gmail.com>
2024-07-17 15:18:20 -0400

    vehicle class updated

--- Vechile_rental_management/Vehicle.cs
+++ Vechile_rental_management/Vehicle.cs
@@ -2,22 +2,21 @@
 
 public class Vehicle
 {
-    public string Model { get; set; }
-    public string Manufacturer { get; set; }
-    public int Year { get; set; }
-    public decimal RentalPrice { get; set; }
+   public string Model{ get; set; }
+   public string Manufacturer{ get; set; }
+   public int Year{ get; set; }
+   public decimal RentalPrice{ get; set; }
 
-    public Vehicle(string model, string manufacturer, int year, decimal rentalPrice)
-    {
-        Model = model;
-        Manufacturer = manufacturer;
-        Year = year;
-        RentalPrice = rentalPrice;
-    }
-
+   public Vehicle(string model, string manufacturer, int year, decimal rentalPrice)
+   {
+      Model = model;
+      Manufacturer = manufacturer;
+      Year = year;
+      RentalPrice = rentalPrice;
+   }
     public virtual void DisplayDetails()
     {
-        Console.WriteLine("Model: " + Model + ", Manufacturer: " + Manufacturer + ", Year: " + Year + ", Rental Price: " + RentalPrice.ToString("C"));
+      Console.WriteLine("Model: " + Model + ", Manufacturer: " + Manufacturer + ", Year: " + Year + ", Rental Price: " + RentalPrice.ToString("C"));
     }
 }
 
