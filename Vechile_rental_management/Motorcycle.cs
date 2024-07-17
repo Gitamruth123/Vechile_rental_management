da11676db1095a9deda76ed4660e5d0cd13f88f3
Gitamruth123 <amruthav1991@gmail.com>
2024-07-17 15:13:38 -0400

    Add project files.

---
+++ Vechile_rental_management/Motorcycle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vechile_rental_management
+{
+    internal class Motorcycle
+    {
+    }
+}
