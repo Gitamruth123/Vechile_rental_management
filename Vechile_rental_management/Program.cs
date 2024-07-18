e8543f5b9805f949356d26834b985a4bd2b9c163
Gitamruth123 <amruthav1991@gmail.com>
2024-07-18 11:03:31 -0400

    chnages

--- Vechile_rental_management/Program.cs
+++ Vechile_rental_management/Program.cs
@@ -5,9 +5,9 @@ class Program
   {
       RentalAgency rentalAgency = new RentalAgency();
 
-      Car car = new Car { Model = "Jetta", Manufacturer = "Volksvagen", Year = 2021, RentalPrice = 55, Seats = 6, EngineType = "V5", Transmission = "Automatic", Convertible = false };
-      Truck truck = new Truck { Model = "VNR", Manufacturer = "Volvo", Year = 2020, RentalPrice = 85, Capacity = 1000, TruckType = "Pickup", FourWheelDrive = true };
-      Motorcycle motorcycle = new Motorcycle { Model = "Supercub", Manufacturer = "Honda", Year = 2023, RentalPrice = 50, EngineCapacity = 650, FuelType = "Petrol", HasFairing = true };
+      Car car1 = new Car { Model = "Jetta", Manufacturer = "Volksvagen", Year = 2021, RentalPrice = 55, Seats = 6, EngineType = "V5", Transmission = "Automatic", Convertible = false };
+      Truck truck1 = new Truck { Model = "VNR", Manufacturer = "Volvo", Year = 2020, RentalPrice = 85, Capacity = 1000, TruckType = "Pickup", FourWheelDrive = true };
+      Motorcycle motorcycle1 = new Motorcycle { Model = "Supercub", Manufacturer = "Honda", Year = 2023, RentalPrice = 50, EngineCapacity = 650, FuelType = "Petrol", HasFairing = true };
 
       rentalAgency.AddVehicle(car);
       rentalAgency.AddVehicle(truck);
