b2651bd323d8f9b6c599cdbcfa55e5c8f5b3bfd6
Gitamruth123 <amruthav1991@gmail.com>
2024-07-17 23:35:23 -0400

    Motocycle class updated

--- Vechile_rental_management/Motorcycle.cs
+++ Vechile_rental_management/Motorcycle.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 public class Motorcycle : Vehicle
 {
-    public int EngineCapacity { get; set; }
-    public string FuelType { get; set; }
-    public bool HasFairing { get; set; }
+  public int EngineCapacity{ get; set; }
+  public string FuelType{ get; set; }
+  public bool HasFairing{ get; set; }
 
-    public override void DisplayDetails()
-    {
-        base.DisplayDetails();
-        Console.WriteLine("Engine Capacity: " + EngineCapacity);
-        Console.WriteLine("Fuel Type: " + FuelType);
-        Console.WriteLine("Has Fairing: " + HasFairing);
-    }
+  public override void DisplayDetails()
+  {
+      base.DisplayDetails();
+      Console.WriteLine("Engine Capacity: " + EngineCapacity);
+      Console.WriteLine("Fuel Type: " + FuelType);
+      Console.WriteLine("Has Fairing: " + HasFairing);
+  }
 }
