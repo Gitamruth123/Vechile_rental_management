a2d5361e171669b95ee7e955a47efc47125936eb
Gitamruth123 <amruthav1991@gmail.com>
2024-07-17 23:29:47 -0400

    Car class updated

--- Vechile_rental_management/RentalAgency.cs
+++ Vechile_rental_management/RentalAgency.cs
@@ -1,38 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
 public class RentalAgency
 {
-  private List<Vehicle> Fleet{ get; set; }
-  public decimal TotalRevenue{ get; private set; }
+    private List<Vehicle> Fleet { get; set; } = new List<Vehicle>();
+    public decimal TotalRevenue { get; private set; }
 
-  public RentalAgency()
-  {
-     Fleet = new List<Vehicle>();
-     TotalRevenue = 0;
-  }
+    public void AddVehicle(Vehicle vehicle)
+    {
+        Fleet.Add(vehicle);
+        
+    }
 
-  public void AddVehicle(Vehicle vehicle)
-  {
-     Fleet.Add(vehicle);
-     Console.WriteLine(vehicle.Model + " added to fleet.");
-  }
+    public void RemoveVehicle(Vehicle vehicle)
+    {
+        Fleet.Remove(vehicle);
+    }
 
-  public void RemoveVehicle(Vehicle vehicle)
-  {
-     Fleet.Remove(vehicle);
-     Console.WriteLine(vehicle.Model + " removed from fleet.");
-  }
-  public void RentVehicle(Vehicle vehicle, int days)
-  {
-     if (Fleet.Contains(vehicle))
-     {
-         decimal rentalCost = vehicle.RentalPrice * days;
-         TotalRevenue += rentalCost;
-         Console.WriteLine(vehicle.Model + " rented for " + days + " days at a cost of " + rentalCost.ToString("C") + ". Total revenue: " + TotalRevenue.ToString("C"));
-     }
-     else
-     {
-        Console.WriteLine(vehicle.Model + " is not available in the fleet.");
-     }
-  }
+    public void RentVehicle(Vehicle vehicle, int days)
+    {
+        if (Fleet.Contains(vehicle))
+        {
+            TotalRevenue += vehicle.RentalPrice * days;
+            Fleet.Remove(vehicle);
+            Console.WriteLine(vehicle.Model + " has been rented for " + days + " days.");
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine(vehicle.Model + " is not available.");
+        }
+    }
+
+    public void DisplayFleet()
+    {
+        Console.WriteLine("Current Fleet:");
+        Console.WriteLine();
+        foreach (var vehicle in Fleet)
+        {
+            vehicle.DisplayDetails();
+            Console.WriteLine("---------------------------");
+        }
+    }
 }
