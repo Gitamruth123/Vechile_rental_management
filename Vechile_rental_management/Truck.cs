f73107521c4eebb433139833b6a88e2f2501df93
Gitamruth123 <amruthav1991@gmail.com>
2024-07-17 23:33:04 -0400

    Truck class updated

--- Vechile_rental_management/Truck.cs
+++ Vechile_rental_management/Truck.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 public class Truck : Vehicle
 {
-    public int Capacity { get; set; }
-    public string TruckType { get; set; }
-    public bool FourWheelDrive { get; set; }
+  public int Capacity{ get; set; }
+  public string TruckType{ get; set; }
+  public bool FourWheelDrive{ get; set; }
 
-    public override void DisplayDetails()
-    {
-        base.DisplayDetails();
-        Console.WriteLine("Capacity: " + Capacity);
-        Console.WriteLine("Truck Type: " + TruckType);
-        Console.WriteLine("Four Wheel Drive: " + FourWheelDrive);
-    }
+  public override void DisplayDetails()
+  {
+      base.DisplayDetails();
+      Console.WriteLine("Capacity: " + Capacity);
+      Console.WriteLine("Truck Type: " + TruckType);
+      Console.WriteLine("Four Wheel Drive: " + FourWheelDrive);
+  }
 }
